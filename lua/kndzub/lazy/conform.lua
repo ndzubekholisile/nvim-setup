85ea30307e131ddb5aab189783fc1f690cf9b426
ndzubekholisile <ndzubekholisile@gmail.com>
2026-02-14 14:01:56 +0000

    live file formatting works: finishing touches

--- lua/kndzub/lazy/conform.lua
+++ lua/kndzub/lazy/conform.lua
@@ -1,27 +1,28 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            "<leader>f",
-            function() require("conform").format({ async = true, lsp_fallback = true }) end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            -- Use a sub-list to run only the first available formatter
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-            typescript = { "prettierd", "prettier", stop_after_first = true },
-            python = { "isort", "black" },
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
-    },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Use a sub-list to run only the first available formatter
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			python = { "black" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
-
--- lua/kndzub/lazy/mason.lua
+++ lua/kndzub/lazy/mason.lua
@@ -1,37 +1,30 @@
-
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "vtsls" }, -- Auto-install these
-            })
-
-        end,
-    },
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        config = function()
-            require("mason-tool-installer").setup({
-                ensure_installed = {
-                    -- LSPs (for mason-lspconfig)
-                    "lua_ls",
-                    "vtsls",
-                    -- Formatters/Linters (not LSPs)
-                    "stylua",
-                    "isort",
-                    "black",
-                    "prettier",
-                },
-            })
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "vtsls" }, -- Auto-install these
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- Formatters/Linters (not LSPs)
+					"stylua",
+					"prettier",
+				},
+			})
+		end,
+	},
 }
