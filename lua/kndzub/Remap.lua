ed25d5f0929957caf850d5dbdd805b0d6836a380
ndzubekholisile <=>
2026-02-18 14:36:23 +0000

    auto close tag works

--- lua/kndzub/Remap.lua
+++ lua/kndzub/Remap.lua
@@ -1,2 +1,8 @@
 -- Open Netrw in the directory of the current file
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>rn", function()
+	-- it returns success status, thus you can fallback like so
+	if not require("ts-autotag").rename() then
+		vim.lsp.buf.rename()
+	end
+end)
